fc2347b89c96082c151aeffd1874e671049ddb21
dul800 <d.ural@student.vu.nl>
2024-07-15 22:15:09 +0200

    Update evaluation.py

--- birbs/col_filtering/evalutation.py
+++ birbs/col_filtering/evalutation.py
@@ -11,6 +11,14 @@ def get_number_of_peers_in_list():
     This function returns the number of peers in the list.
     """
 
+    # if the whitelist file does not exist, return 0
+    if not os.path.exists("resources/whitelist/whitelist.json"):
+        return 0
+    
+    # If the whitelist file is empty, return 0
+    if os.stat("resources/whitelist/whitelist.json").st_size == 0:
+        return 0
+
     # Opening whitelist which is a json
     with open("resources/whitelist/whitelist.json", "r", encoding="utf-8") as file:
         whitelist = json.load(file)
@@ -45,4 +53,4 @@ def evaluate_receiving(start_time: float, message_size: int, end_time: float, fr
             file.write("start_time,end_time,delta,elapsed_time,message_size,sending,from,to,num_peers\n")
     
     with open("resources/data/evaluation.csv", "a", encoding="utf-8") as file:
-        file.write(f"{start_time},{end_time},{" "},{elapsed_time},{message_size},{False},{from_},{to_},{get_number_of_peers_in_list()}\n")
+        file.write(f"{start_time},{end_time}, ,{elapsed_time},{message_size},{False},{from_},{to_},{get_number_of_peers_in_list()}\n")
